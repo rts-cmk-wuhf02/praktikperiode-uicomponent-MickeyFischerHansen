e79f9061a11ad63471ba90b692917f1b6fc48e6b
Mickey Fischer Hansen <4335582@RTS-365.dk>
2020-06-02 14:14:16 +0200

    push

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,13 +2,19 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Maledives from './component/malediv/malediv.js';
+import Outdoor from './component/outdoor/outdoor.js';
 
 function App() {
   return (
     <div className="App">
       <Maledives />
+      <Outdoor />
     </div>
   );
+  
 }
 
+
+
 export default App;
+
